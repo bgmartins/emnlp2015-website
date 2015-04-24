7187cdbaf891ed0abeb43ee066d9be5799f1d43d
Bruno Martins <bgmartins@gmail.com>
2015-04-24 09:45:00 +0100

    Changes in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 header("HTTP/1.1 301 Moved Permanently"); 
-header("Location: http://www.emnlp2015.org/index.html"); 
+header("Location: http://www.emnlp2015.org/"); 
 ?>
 <?php include_once("index.html"); ?>
